6bfb01b570f256372336a1b49749c22b0bf18c45
Javier <Jvasquez5422@pelhamprepacademy.org>
2023-08-09 15:06:23 -0400

    Added all Files

---
+++ innit-firebase.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDPqswaFzpyFrZj7t6KrHkHiONslU4Q-bo",
+    authDomain: "blogpost-14bb5.firebaseapp.com",
+    projectId: "blogpost-14bb5",
+    storageBucket: "blogpost-14bb5.appspot.com",
+    messagingSenderId: "1013583451979",
+    appId: "1:1013583451979:web:55bfc257c7cf5f7f63bace",
+    measurementId: "G-SBZXLCJL47"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  
